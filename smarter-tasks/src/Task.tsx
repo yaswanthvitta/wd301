095be3faaf5e6780a5593d9aa456c4380ba49818
root <root@Yaswanth2003.localdomain>
2024-03-31 16:04:44 +0530

    level4

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -21,7 +21,7 @@ interface TaskProp {
       </div>
       <div style={{textAlign:"center",width:"40%",display:"flex",marginTop:"12px",flexDirection:"column",justifyContent:"center"}}>
         <div style={{width:"100%",textAlign:"center",height:"50px",display:"flex",justifyContent:"center"}}>
-        <button onClick={deleteTask} id="deleteTaskButton" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style={{height:"40px",textAlign:"center",width:"100px"}}>delete</button>
+        <button onClick={deleteTask} id="deleteTaskButton" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 deleteTaskButton"    style={{height:"40px",textAlign:"center",width:"100px"}}>delete</button>
         </div>
       </div>
       </div>
