945ff352dc22c2acb214c4b34dfeea4e66a4be6c
root <root@Yaswanth2003.localdomain>
2024-04-05 22:29:52 +0530

    level5

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -118,7 +118,7 @@ const TaskForm = (props: TaskFormProps) => {
         <div className="relative z-0 w-full mb-6 group">
           <button
             type="submit"
-            id="#addTaskButton"
+            id="addTaskButton"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Add item
