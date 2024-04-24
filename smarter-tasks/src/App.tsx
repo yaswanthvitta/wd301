60b51eca4776c1ecbf6b0cdad04e7a4e2bd3a54b
root <root@Yaswanth2003.localdomain>
2024-04-24 19:19:37 +0530

    level7

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import  { useContext } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes"
